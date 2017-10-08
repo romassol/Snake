d1ab8ce9fcd8ba9ab2cdda27b31e9e9a31e0d839
romassol <romass1998@mail.ru>
2017-10-08 20:46:19 +0500

    add generate in AppleGenerator

--- src/snake/AppleGenerator.java
+++ src/snake/AppleGenerator.java
@@ -1,6 +1,8 @@
 package snake;
 
+import java.util.ArrayList;
 import java.util.Objects;
+import java.util.Random;
 
 public class AppleGenerator {
     private Integer applesCount;
@@ -14,7 +16,28 @@ public class AppleGenerator {
     }
 
     public void generate(Level level){
-        
-        applesCount--;
+        while(applesCount > 0){
+            ArrayList<Vector> indexesFreeCells = getAllFreeCells(level);
+            Random random = new Random();
+            Integer index = random.nextInt(indexesFreeCells.size());
+            Vector randomFreeCells = indexesFreeCells.get(index);
+            Integer x = randomFreeCells.DELTA_X;
+            Integer y = randomFreeCells.DELTA_Y;
+            level.objects[x][y] = new Apple(x, y);
+            applesCount--;
+        }
+
+    }
+
+    private ArrayList<Vector> getAllFreeCells(Level level) {
+        ArrayList<Vector> indexesFreeCells = new ArrayList<>();
+        for (int i = 0; i < level.objects.length; i++){
+            for (int j = 0; j < level.objects[i].length; j++){
+                if(level.objects[i][j] instanceof Empty){
+                    indexesFreeCells.add(new Vector(j, i));
+                }
+            }
+        }
+        return indexesFreeCells;
     }
 }
