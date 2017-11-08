5352744f54d6d7c5b89555471f9e4e589da294cb
DerendeevaNastya <32324515+DerendeevaNastya@users.noreply.github.com>
2017-11-08 21:27:38 +0400

    вроде дописала, но надо запустить и посмотреть. немножко изменила fillHorizontal и Vertical

--- src/snake/LevelGenerator.java
+++ src/snake/LevelGenerator.java
@@ -1,7 +1,12 @@
 package snake;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -10,17 +15,31 @@ import static java.lang.Math.min;
 
 public class LevelGenerator {
 
-    public LevelGenerator(){
-    }
-
     private Random random = new Random();
 
 
-    public Level createAndGetLevel(String fileName, int applesCount) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IOException, IllegalAccessException {
+    public Level createAndGetLevel(String fileName, int applesCount)
+            throws InvocationTargetException,
+            NoSuchMethodException,
+            InstantiationException,
+            IOException,
+            IllegalAccessException {
         int fieldHeight = random.nextInt(40) + 10;
         int fieldWidth = random.nextInt(40) + 10;
         String[][] field = getStringField(fieldHeight, fieldWidth);
+        createLevelFile(fileName, field);
         return new Level(new FieldReader(fileName), applesCount);
+    }
+
+    private void createLevelFile(String fileName, String[][] field) throws IOException {
+        File levelFile = new File(Settings.LEVEL_URL +fileName);
+        FileWriter writeFile = new FileWriter(levelFile);
+        String strField = "";
+        for (String[] line : field){
+            strField += String.join("", line);
+            strField += '\n';
+        }
+        writeFile.append(strField);
     }
 
     private String[][] getStringField(int fieldHeight, int fieldWidth){
@@ -28,23 +47,44 @@ public class LevelGenerator {
         List<Cell> cells = new ArrayList<>();
         fillCells(fieldWidth, fieldHeight, cells);
         setInternalCell(cells);
-//
+        fillField(field, cells);
         return field;
+    }
+
+    private void fillField(String[][] field, List<Cell> cells){
+        for (Cell cell : cells){
+            Cell room = cell.internalCell;
+            for (int x = room.getTopLeft().x; x <= room.getBottomRight().x; x++){
+                field[x][room.getTopLeft().y] = "#";
+                field[x][room.getBottomRight().y] = "#";
+            }
+            for (int y = room.getTopLeft().y; y < room.getBottomRight().y; y++){
+                field[room.getTopLeft().x][y] = "#";
+                field[room.getBottomRight().x][y] = "#";
+            }
+        }
     }
 
     private void fillCells(int fieldWidth, int fieldHeight, List<Cell> cells){
         int cellSize = random.nextInt(min(fieldWidth, fieldHeight)) + 5;
         int numberOfCellsVertically = fieldHeight / cellSize;
+        int remainder = fieldHeight % cellSize;
         for (int i = 0; i < numberOfCellsVertically; i++){
-            fillHorizontal(i, cellSize, cellSize, fieldWidth, cells);
+            if (i == numberOfCellsVertically - 1)
+                fillHorizontal(i, cellSize + remainder, cellSize, fieldWidth, cells);
+            else
+                fillHorizontal(i, cellSize, cellSize, fieldWidth, cells);
         }
     }
 
     private void fillHorizontal(int numberOfRow, int heightOfCell, int widthOfCell, int fieldWidth, List<Cell> cells) {
         int numberOfCellsHorizontally = fieldWidth / widthOfCell;
+        int remainder = fieldWidth % widthOfCell;
         for (int j = 0; j < numberOfCellsHorizontally; j++){
             Vector topLeft = new Vector(j*widthOfCell, numberOfRow*widthOfCell);
             Vector bottomRight = topLeft.sum(new Vector(widthOfCell - 1, heightOfCell - 1));
+            if (j == numberOfCellsHorizontally - 1)
+                bottomRight = topLeft.sum(new Vector(widthOfCell + remainder - 1, heightOfCell - 1));
             cells.add(new Cell(topLeft, bottomRight, null));
         }
     }
@@ -62,9 +102,5 @@ public class LevelGenerator {
                 }
             }
         }
-    }
-
-    public static void main(String[] args){
-
     }
 }
